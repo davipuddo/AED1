05cacfce93f5d52a2be8bff7e150c1a366d4bdbd
davif <davifpuddo@gmail.com>
2024-05-24 13:59:49 -0300

    ED1204

--- C++/ED12/ED12.cpp
+++ C++/ED12/ED12.cpp
@@ -7,7 +7,7 @@
 void ED0X01 (void)
 {
   // Identificacao de dados
-  Matrix <int> matrix(3,3);
+  Matrix <int> matrix(2, 2);
   int n = 0;
 
   // Apresentacao
@@ -33,9 +33,16 @@ void ED0X01 (void)
     }
     else
     {
-      // Mostrar matrix
+      // Converter array para matrix
       matrix = array;
+
+      // Mostrar matrix
+      println ("Matrix -");
       matrix.print();
+
+      // Liberar dados da memoria
+      array.free();
+      matrix.free();
     }
   }
 
@@ -48,10 +55,10 @@ void ED0X01 (void)
 void ED0X02 (void)
 {
   // Identificacao de dados
-  int linhas  = 0;
-  int colunas = 0;
+  Matrix <int> Fmatrix(0, 0);
+  Matrix <int> resultado(0, 0);
+  Matrix <int> matrix(0, 0);
   int constante = 0;
-  Matrix <int> resultado(2,2);
 
   // Apresentacao
   println ("ED0X02");
@@ -60,25 +67,31 @@ void ED0X02 (void)
   println ("");                  // Pular uma linha
     
   // Ler dados
-  constante = ReadInt("Forneca a constante para definir o escalamento da matrix: ");
-  linhas  = ReadPositiveInt("Forneca a quantidade de linhas da matrix:  ");
-  colunas = ReadPositiveInt("Forneca a quantidade de colunas da matrix: ");
-  Matrix <int> matrix(linhas, colunas);
+  Fmatrix.SetSize();
+  Fmatrix.write();
+
+  // Escrever matrix no arquivo
+  Fmatrix.fwrite("DADOS02.TXT");
+
+  // Liberar matrix da memoria
+  Fmatrix.free();
+
+  // Ler matrix do arquivo
+  matrix.fread("DADOS02.TXT");
 
   // Verificar dados
   if (matrix.IsValid())
-  {
-    // Ler valores para a matrix
-    matrix.write();
-    
+  {   
     // Verificar dados
     if (matrix.IsValid())
     {
       // Escalar matrix
+      constante = ReadInt("Forneca a constante para definir o escalamento da matrix: ");
       resultado = matrix * constante;
       if (resultado.IsValid())
       {
         // Mostrar dados
+        println ("Matrix escalada -");
         resultado.print();
       }
     }
@@ -96,8 +109,8 @@ void ED0X02 (void)
 void ED0X03 (void)
 {
   // Identificacao de dados
-  int linhas = 0;
-  int colunas = 0;
+  Matrix <int> Fmatrix(0, 0);
+  Matrix <int> matrix(0, 0);
   bool resultado = false;
 
   // Apresentacao
@@ -106,35 +119,56 @@ void ED0X03 (void)
   println (" ");
   println ("");                  // Pular uma linha
     
-  // Ler dados
-  linhas  = ReadPositiveInt("Forneca a quantidade de linhas da matrix: ");
-  colunas = ReadPositiveInt("Forneca a quantidade de colunas da matrix: ");
-  Matrix <int> matrix(linhas, colunas);
+  // Ler dimensoes da matrix
+  Fmatrix.SetSize();
 
-  // Verificar dados
-  if (matrix.getrows() > 0 && matrix.getcolumns() > 0)
+  // Verificar dimensoes
+  if (Fmatrix.getcolumns() <= 0 || Fmatrix.getrows() <= 0)
+  {
+    println ("ERRO: Dimensoes invalidas. ");
+  }
+  else
   {
     // Ler valores para a matrix
-    matrix.write();
+    Fmatrix.write();
 
-    if (matrix.IsValid())
+    // Verificar dados
+    if (!Fmatrix.IsValid())
     {
-      // Verificar se a matrix e a matrix identidade
-      resultado = matrix.CheckIdentity();
-
-      // Mostrar dados
-      if (resultado)
-      {
-        println ("A matrix fornecida e a matrix identidade. ");
-      }
-      else
-      {
-        println ("A matrix fornecida nao e a matrix identidade. ");
-      }
-      matrix.print();
+      println ("ERRO: Dados invalidos. ");
+    }
+    else
+    {
+      // Escrever dados no arquivo
+      Fmatrix.fwrite("DADOS03.TXT");
 
       // Liberar matrix da memoria
-      matrix.free();
+      Fmatrix.free();
+
+      // Ler dados do arquivo
+      matrix.fread("DADOS03.TXT");
+
+      // Verificar dimensoes
+      if (matrix.IsValid())
+      {
+        // Verificar se a matrix e a matrix identidade
+        resultado = matrix.CheckIdentity();
+
+        // Mostrar dados
+        if (resultado)
+        {
+          println ("A matrix fornecida e a matrix identidade. ");
+        }
+        else
+        {
+          println ("A matrix fornecida nao e a matrix identidade. ");
+        }
+        println ("Matrix -");
+        matrix.print();
+
+        // Liberar matrix da memoria
+        matrix.free();
+      }
     }
   }
 
@@ -147,15 +181,94 @@ void ED0X03 (void)
 void ED0X04 (void)
 {
   // Identificacao de dados
+  Matrix <int> Fmatrix1(0, 0);
+  Matrix <int> Fmatrix2(0, 0);
+  Matrix <int> matrix1(0, 0);
+  Matrix <int> matrix2(0, 0);
+  bool resultado = false;
+
   // Apresentacao
   println ("ED0X04");
   println ("");                  // Pular uma linha
   println (" ");
   println ("");                  // Pular uma linha
     
-  // Ler dados
-  // Verificar dados
-  // Mostrar dados
+  // Ler dimensoes da matrix
+  Fmatrix1.SetSize();
+
+  // Verificar dimensoes
+  if (Fmatrix1.getdimensions() <= 0)
+  {
+    println ("ERRO: Dimensoes invalidas. ");
+  }
+  else
+  {
+    // Ler valores para a matrix 
+    Fmatrix1.write();
+
+    // Ler tamanho da matrix
+    Fmatrix2.SetSize();
+
+    // Verificar dimensoes
+    if (Fmatrix2.getdimensions() <= 0)
+    {
+      println ("ERRO: Dimensoes invalidas. ");
+    }
+    else
+    {
+      // Ler valores para a matrix
+      Fmatrix2.write();
+
+      // Verificar dados
+      if (!Fmatrix1.IsValid() || !Fmatrix2.IsValid())
+      {
+        println ("ERRO: Dados invalidos. ");
+      }
+      else
+      {
+        // Escrever matrizes nos arquivos
+        Fmatrix1.fwrite("DADOS0401.TXT");
+        Fmatrix2.fwrite("DADOS0402.TXT");
+
+        // Liberar matrizes da memoria
+        Fmatrix1.free();
+        Fmatrix2.free();
+
+        // Ler matrizes dos arquivos
+        matrix1.fread("DADOS0401.TXT");
+        matrix2.fread("DADOS0402.TXT");
+
+        // Verificar dados
+        if (!matrix1.IsValid() || !matrix2.IsValid())
+        {
+          println ("ERRO: Dados invalidos. ");
+        }
+        else
+        {
+          // Verificar igualdade das matrizes
+          resultado = matrix1 == matrix2;
+
+          // Mostrar dados
+          if (resultado)
+          {
+            println ("As matrizes sao iguais. ");
+          }
+          else
+          {
+            println ("As matrizes sao diferentes. ");
+          }
+          println ("Matriz 1 -");
+          matrix1.print();
+          println ("Matriz 2 -");
+          matrix2.print();
+
+          // Liberar matrizes da memoria
+          matrix1.free();
+          matrix2.free();
+        }
+      }
+    }
+  }
 
   // Fim
   println ("");                  // Pular uma linha
