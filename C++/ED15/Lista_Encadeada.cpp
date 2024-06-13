dbd2f9285469cca78727487d5b8bae1ec9dcc70e
davipuddo <davifpuddo@gmail.com>
2024-06-13 07:35:59 -0300

    Lista Encadeada

--- C++/ED15/Lista_Encadeada.cpp
+++ C++/ED15/Lista_Encadeada.cpp
@@ -70,7 +70,8 @@ class cell
         }
         else
         {
-           link = new cell<T> (value, null);
+            link = new cell<T> (value, null);
+            std::cout << "added: " << link->data << std::endl;
         }
     }
 
@@ -84,6 +85,7 @@ class cell
             }
             else
             {
+                std::cout << "removed: " << link->data << std::endl;
                 link->data = 0;
                 link = null;
             }
@@ -94,6 +96,7 @@ class cell
     {
         link = new cell<T> (data, link);
         data = value;
+        std::cout << "added: " << data << std::endl;
     }
 
     void PopFront ()
@@ -107,9 +110,63 @@ class cell
             }
             else
             {
+                std::cout << "removed: " << link->data << std::endl;
                 link->data = 0;
                 link = null;
             }
+        }
+    }
+
+    int Count (int x)
+    {
+        x++;
+        if (link)
+        {
+            return this->link->Count(x);
+        }
+        else
+        {
+            return (x);
+        }
+    }
+
+    void PushMid (int value)
+    {
+        cell *ptr = null;
+        if (link)
+        {
+            int c = this->Count(0);
+            int mid = ((double)c / 2.0);
+            ptr = this;
+            
+            int w = (mid-2);
+            for (int i = 0; i < w; i++)
+            {
+                ptr = ptr->link;
+            }
+            cell *b = new cell<T>(value, ptr->link->link);
+            ptr->link->link = b;
+            std::cout << "added: " << link->link->data << std::endl;
+        }
+    }
+
+    void PopMid ()
+    {
+        cell *ptr = null;
+        if (link)
+        {
+            int c = this->Count(0);
+            int mid = ((double)c / 2.0);
+            ptr = this;
+            
+            int w = (mid-1);
+            for (int i = 0; i < w; i++)
+            {
+                ptr = ptr->link;
+            }
+            std::cout << "removed: " << ptr->link->data << std::endl;
+            ptr->link->data = 0;
+            ptr->link = ptr->link->link; 
         }
     }
 };
@@ -136,6 +193,7 @@ void m_01 (void)
         back = b;
     }
     a->print();
+    std::cout << a->Count(0) << std::endl;
 
     a->free();
     a = null;
@@ -156,7 +214,7 @@ void m_02 (void)
     a = b;
     back = a;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 3; i++)
     {
         b = new cell<char>(ReadChar(""), null);
 
@@ -171,6 +229,12 @@ void m_02 (void)
     a->PushFront('@');
     a->print();
     a->PopFront();
+    a->print();
+    a->PopBack();
+    a->print();
+    a->PushMid('@');
+    a->print();
+    a->PopMid();
     a->print();
 
     a->free();
